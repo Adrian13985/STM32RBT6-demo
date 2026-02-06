f1b4adde51688b2f41e4eb0ac863d996875d2803
Adrian <frankadrian985@gmail.com>
2026-02-06 13:28:13 +0800

    add ADC

---
+++ ADC/Core/Inc/fonts.h
@@ -0,0 +1,23 @@
+/*
+  ����˵��: CT117EǶ��ʽ������LCD��������
+  �������: Keil uVision 4.10
+  Ӳ������: CT117EǶ��ʽ������
+  ��    ��: 2011-8-9
+*/
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __FONTS_H
+#define __FONTS_H
+
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "lcd.h"
+
+/* Exported types ------------------------------------------------------------*/
+/* ASCII Table: each character is 16 column (16dots large)
+   and 24 raw (24 dots high) */
+extern const uc16 ASCII_Table[];
+
+#endif
+
+/* __FONTS_H */
